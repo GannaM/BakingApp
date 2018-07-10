9edd186cb68028783e9423d4e43100a9e354d4ac
Anna Myshliakova <ganna.myshliakova@gmail.com>
2018-07-10 16:14:40 -0700

    Set GridLayout for a Tablet

--- app/src/main/java/com/example/android/bakingapp/MainActivity.java
+++ app/src/main/java/com/example/android/bakingapp/MainActivity.java
@@ -1,10 +1,11 @@
 package com.example.android.bakingapp;
 
+import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.widget.ListView;
 
 import com.example.android.bakingapp.RecipeAdapter.RecipeAdapterOnClickHandler;
 import com.example.android.bakingapp.model.Recipe;
@@ -22,13 +23,32 @@ public class MainActivity extends AppCompatActivity implements RecipeAdapterOnCl
         List<Recipe> recipeList = JsonUtils.parseRecipeJson(this);
 
         RecyclerView recipeRecyclerView = findViewById(R.id.recipes_recyclerview);
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
-        recipeRecyclerView.setLayoutManager(layoutManager);
-
-        //RecipeAdapterOnClickHandler clickHandler = (RecipeAdapterOnClickHandler) this;
         RecipeAdapter recipeAdapter = new RecipeAdapter(this, getApplicationContext());
         recipeRecyclerView.setAdapter(recipeAdapter);
         recipeAdapter.setRecipeData(recipeList);
+
+        boolean isTablet = getResources().getBoolean(R.bool.isTablet);
+
+        if (isTablet) {
+            GridLayoutManager gridLayoutManager;
+
+            int orientation = getResources().getConfiguration().orientation;
+            if (orientation == Configuration.ORIENTATION_PORTRAIT) {
+                gridLayoutManager = new GridLayoutManager(this, 2);
+            }
+            else {
+                gridLayoutManager = new GridLayoutManager(this, 3);
+            }
+            recipeRecyclerView.setLayoutManager(gridLayoutManager);
+        }
+        else {
+            LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+            recipeRecyclerView.setLayoutManager(linearLayoutManager);
+        }
+
+
+        //RecipeAdapterOnClickHandler clickHandler = (RecipeAdapterOnClickHandler) this;
+
 
 
 
