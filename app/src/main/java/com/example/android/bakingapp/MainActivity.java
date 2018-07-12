3ce3415c32a6a8c9570706d2a3422687c79e2c1b
Anna Myshliakova <ganna.myshliakova@gmail.com>
2018-07-12 15:45:07 -0700

    Added Butternife Added ErrorMessageDisplay and LoadingIndicator

--- app/src/main/java/com/example/android/bakingapp/MainActivity.java
+++ app/src/main/java/com/example/android/bakingapp/MainActivity.java
@@ -7,6 +7,9 @@ import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.example.android.bakingapp.RecipeAdapter.RecipeAdapterOnClickHandler;
 import com.example.android.bakingapp.model.Recipe;
@@ -19,14 +22,19 @@ import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity implements RecipeAdapterOnClickHandler {
 
-    private List<Recipe> mRecipeList;
+    @BindView(R.id.pb_loading_indicator) ProgressBar mLoadingIndicator;
+    @BindView(R.id.tv_error_message_display) TextView mErrorMessageDisplay;
+    @BindView(R.id.recipes_recyclerview) RecyclerView mRecipeRecyclerView;
 
+    private List<Recipe> mRecipeList;
     private RecipeAdapter mRecipeAdapter;
 
     private static final String LIST_KEY = "recipeList";
@@ -35,10 +43,10 @@ public class MainActivity extends AppCompatActivity implements RecipeAdapterOnCl
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
-        RecyclerView recipeRecyclerView = findViewById(R.id.recipes_recyclerview);
         mRecipeAdapter = new RecipeAdapter(this, getApplicationContext());
-        recipeRecyclerView.setAdapter(mRecipeAdapter);
+        mRecipeRecyclerView.setAdapter(mRecipeAdapter);
 
         if (savedInstanceState != null) {
             String recipeJson = savedInstanceState.getString(LIST_KEY);
@@ -47,6 +55,8 @@ public class MainActivity extends AppCompatActivity implements RecipeAdapterOnCl
         }
 
         else {
+            mLoadingIndicator.setVisibility(View.VISIBLE);
+            mRecipeRecyclerView.setVisibility(View.INVISIBLE);
             fetchDataAndPopulateUI();
         }
 
@@ -61,11 +71,11 @@ public class MainActivity extends AppCompatActivity implements RecipeAdapterOnCl
             else {
                 gridLayoutManager = new GridLayoutManager(this, 3);
             }
-            recipeRecyclerView.setLayoutManager(gridLayoutManager);
+            mRecipeRecyclerView.setLayoutManager(gridLayoutManager);
         }
         else {
             LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
-            recipeRecyclerView.setLayoutManager(linearLayoutManager);
+            mRecipeRecyclerView.setLayoutManager(linearLayoutManager);
         }
 
     }
@@ -118,13 +128,29 @@ public class MainActivity extends AppCompatActivity implements RecipeAdapterOnCl
 
                 mRecipeList = response.body();
                 mRecipeAdapter.setRecipeData(mRecipeList);
+
+                showRecipeView();
             }
 
             @Override
             public void onFailure(Call<List<Recipe>> call, Throwable t) {
                 call.cancel();
+                showErrorMessage();
             }
         });
+    }
+
+    private void showErrorMessage() {
+        mLoadingIndicator.setVisibility(View.INVISIBLE);
+        mRecipeRecyclerView.setVisibility(View.INVISIBLE);
+        mErrorMessageDisplay.setVisibility(View.VISIBLE);
+    }
+
+    private void showRecipeView() {
+        mLoadingIndicator.setVisibility(View.INVISIBLE);
+        mErrorMessageDisplay.setVisibility(View.INVISIBLE);
+        mRecipeRecyclerView.setVisibility(View.VISIBLE);
+
     }
 
 
